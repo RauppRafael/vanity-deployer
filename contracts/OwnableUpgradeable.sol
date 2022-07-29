fc0cb463f12ca5e12b741a5c3ffebb2d110c95a8
RauppRafael <>
2022-07-29 14:19:08 -0300

    fix: OwnableUpgradeable imports

--- contracts/OwnableUpgradeable.sol
+++ contracts/OwnableUpgradeable.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable-v4/access/OwnableUpgradeable.sol" as OzOwnableUpgradeable;
-import "@openzeppelin/contracts-upgradeable-v4/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol" as OzOwnableUpgradeable;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 abstract contract OwnableUpgradeable is OzOwnableUpgradeable.OwnableUpgradeable, UUPSUpgradeable {
     function _authorizeUpgrade(address) internal override onlyOwner {}

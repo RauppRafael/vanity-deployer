edd121f2bcb353a6f294718fb576e8f4885fd3af
RauppRafael <raupperino@gmail.com>
2023-03-09 15:00:32 -0300

    chore: reduced optimizer runs

--- src/helpers/artifacts.ts
+++ src/helpers/artifacts.ts
@@ -1,7 +1,7 @@
 import _VanityDeployer from '../../artifacts/contracts/VanityDeployer.sol/VanityDeployer.json'
 import _VanityProxy from '../../artifacts/contracts/VanityProxy.sol/VanityProxy.json'
 import _ERC1967Proxy from '../../artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json'
-import buildInfo from '../../artifacts/build-info/440e3392007c5cb479b0bce59a51e226.json'
+import buildInfo from '../../artifacts/build-info/d0ea69414a4746927eea58996c3441ca.json'
 
 export const VanityDeployer = _VanityDeployer
 export const VanityProxy = _VanityProxy

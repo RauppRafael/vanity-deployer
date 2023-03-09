e5d82076d1ec68fac610db8308dbf117ac453890
RauppRafael <raupperino@gmail.com>
2023-03-09 19:50:38 -0300

    chore: removed old artifacts

--- src/helpers/artifacts.ts
+++ src/helpers/artifacts.ts
@@ -1,9 +1,3 @@
-import VanityDeployer from '../../artifacts/contracts/VanityDeployer.sol/VanityDeployer.json'
-import VanityProxy from '../../artifacts/contracts/VanityProxy.sol/VanityProxy.json'
-import ERC1967Proxy from '../../artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json'
-import buildInfo from '../../artifacts/build-info/bc6f0320137c2592591dc8a15dceeeff.json'
+import ERC1967Proxy from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json'
 
-export const VanityDeployerArtifact = VanityDeployer
-export const VanityProxyArtifact = VanityProxy
 export const ERC1967ProxyArtifact = ERC1967Proxy
-export const BuildInfoArtifact = buildInfo
--- src/helpers/factories.ts
+++ src/helpers/factories.ts
@@ -1,21 +1,9 @@
 import hre from 'hardhat'
 import { Signer } from 'ethers'
-import { ERC1967ProxyArtifact, VanityDeployerArtifact, VanityProxyArtifact } from './artifacts'
-
-export const getVanityDeployerFactory = (signer?: Signer) => hre.ethers.getContractFactory(
-    VanityDeployerArtifact.abi,
-    VanityDeployerArtifact.bytecode,
-    signer,
-)
+import { ERC1967ProxyArtifact } from './artifacts'
 
 export const getERC1967ProxyFactory = (signer?: Signer) => hre.ethers.getContractFactory(
     ERC1967ProxyArtifact.abi,
     ERC1967ProxyArtifact.bytecode,
-    signer,
-)
-
-export const getVanityProxyFactory = (signer?: Signer) => hre.ethers.getContractFactory(
-    VanityProxyArtifact.abi,
-    VanityProxyArtifact.bytecode,
     signer,
 )
